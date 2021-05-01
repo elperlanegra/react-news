86b50fd65dae1c07f42a82cf599f10abc768a436
Manuel Duarte <carlosd99m@gmail.com>
2021-05-01 15:00:13 -0600

    Consultando la api y leyendo los resultados :art:

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 
 function App() {
-  
   // definir la categoria y Noticias
-  const [categories, saveCategory] = useState('')
-  
+  const [category, saveCategory] = useState("");
+  const [news, saveNews] = useState([])
+
+  useEffect(() => {
+    const consultAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${category}&apiKey=11c9aec0bb8e4e45bffffa391ecf22c2`;
+
+      const answer = await fetch(url)
+      const news = await answer.json();
+
+      saveNews(news.articles)
+    };
+
+    consultAPI()
+
+  }, [category]);
+
   return (
     <Fragment>
       <Header title="News search engine" />
-
+ 
       <div className="container white">
-        <Formulario 
-          saveCategory={saveCategory}
-        />
+        <Formulario saveCategory={saveCategory} />
       </div>
     </Fragment>
   );
